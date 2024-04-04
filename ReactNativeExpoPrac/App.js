0915ae7c71e2488043a95c3b2f290103f03654c2
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-04-05 01:10:47 +0900

    [ReactNativeExpoPrac]update: react native tutorial - practice counter app using function component

--- ReactNativeExpoPrac/App.js
+++ ReactNativeExpoPrac/App.js
@@ -1,44 +1,28 @@
-import React, { Component } from 'react';
-import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+// React Native Counter Example using Hooks!
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-class App extends Component {
-  state = {
-    count: 0,
-  };
+const App = () => {
+  const [count, setCount] = useState(0);
 
-  onPress = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
+  return (
+    <View style={styles.container}>
+      <Text>You clicked {count} times</Text>
+      <Button
+        onPress={() => setCount(count + 1)}
+        title="Click me!"
+      />
+    </View>
+  );
+};
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.button} onPress={this.onPress}>
-          <Text>Click me</Text>
-        </TouchableOpacity>
-        <View>
-          <Text>You clicked {this.state.count} times</Text>
-        </View>
-      </View>
-    );
-  }
-}
-
+// React Native Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10,
-    marginBottom: 10,
-  },
 });
 
 export default App;
-
