dea9eed9f07fe99b4634743c71134ae0c9ce5da1
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-04-05 01:08:21 +0900

    [ReactNativeExpoPrac]update: react native tutorial - practice counter app using class component

--- ReactNativeExpoPrac/App.js
+++ ReactNativeExpoPrac/App.js
@@ -1,28 +1,44 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+
+class App extends Component {
+  state = {
+    count: 0,
+  };
+
+  onPress = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={this.onPress}>
+          <Text>Click me</Text>
+        </TouchableOpacity>
+        <View>
+          <Text>You clicked {this.state.count} times</Text>
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  center: {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginBottom: 10,
   },
 });
 
-const Greeting = props => {
-  return (
-    <View style={styles.center}>
-      <Text>Hello {props.name}!</Text>
-    </View>
-  )
-}
+export default App;
 
-const LotsOfGreetings = () => {
-  return (
-    <View style={[styles.center, { top: 50 }]}>
-      <Greeting name="Rexxar" />
-      <Greeting name="Jaina" />
-      <Greeting name="Valeera" />
-    </View>
-  );
-};
-
-export default LotsOfGreetings;
