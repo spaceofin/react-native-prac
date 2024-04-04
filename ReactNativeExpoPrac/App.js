f5cce3805fc5a50de45dcda3663e6029acb2a7c1
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-04-05 00:52:33 +0900

    [ReactNativeExpoPrac]update: react native tutorial - practice props

--- ReactNativeExpoPrac/App.js
+++ ReactNativeExpoPrac/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
-const HelloWorldApp = () => {
+const styles = StyleSheet.create({
+  center: {
+    alignItems: 'center',
+  },
+});
+
+const Greeting = props => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Hello, world!</Text>
+    <View style={styles.center}>
+      <Text>Hello {props.name}!</Text>
+    </View>
+  )
+}
+
+const LotsOfGreetings = () => {
+  return (
+    <View style={[styles.center, { top: 50 }]}>
+      <Greeting name="Rexxar" />
+      <Greeting name="Jaina" />
+      <Greeting name="Valeera" />
     </View>
   );
 };
-export default HelloWorldApp;
+
+export default LotsOfGreetings;
